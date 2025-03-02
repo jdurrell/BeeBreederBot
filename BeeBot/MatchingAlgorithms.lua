70c4e78007a273208ba9e509ee6ec9e99b28720f
John Durrell <john.m.durrell@gmail.com>
2025-03-02 10:58:02 -0500

    Refactor matching algorithms to use a generic 'highest score' picker.

--- BeeBot/MatchingAlgorithms.lua
+++ BeeBot/MatchingAlgorithms.lua
@@ -3,41 +3,44 @@ local M = {}
 local MatchingMath = require("BeeBot.MatchingMath")
 
 ---@alias Matcher fun(princessStack: AnalyzedBeeStack, droneStackList: AnalyzedBeeStack[], target: string, cacheElement: BreedInfoCacheElement, traitInfo: TraitInfo): integer
+---@alias ScoreFunction fun(droneStack: AnalyzedBeeStack): number
 
--- Computes the optimal matching of the given princess and set of drones for breeding the target.
----@param princessStack AnalyzedBeeStack
----@param droneStackList AnalyzedBeeStack[]
----@param target string
----@param cacheElement BreedInfoCacheElement  map of target to parent1 to parent2 to breed chance (basically a lookup matrix for breeding chance).
----@param traitInfo TraitInfo
----@return integer droneSlot  The slot of the optimal drone in ANALYZED_DRONE_CHEST.
+-- Matches `princessStack` to the drone with the highest chance to immediately create at least one pure-bred target offspring drone.
+---@type Matcher
 function M.HighestPureBredChance(princessStack, droneStackList, target, cacheElement, traitInfo)
-    -- Determine the drone that has the highest chance of breeding the result when paired with the given princess.
-    -- This is important because we will be making use of ignoble princesses, which start dying after they breed too much.
-    -- Being efficient with their use lowers the numbers of other princesses we have to manually load into the system.
+    return M.GenericHighestScore(
+        droneStackList,
+        function(droneStack)
+            return MatchingMath.CalculateChanceAtLeastOneOffspringIsPureBredTarget(
+                target, princessStack.individual, droneStack.individual, cacheElement, traitInfo
+            )
+        end,
+        1
+    )
+end
+
+-- Generic function to wrap algorithms that calculate a score for each drone and pick the one with the highest score.
+---@param scoreFunc ScoreFunction
+---@param maxPossibleScore number | nil
+function M.GenericHighestScore(droneStackList, scoreFunc, maxPossibleScore)
     local maxDroneStack
-    local maxChance = 0.0
+    local maxScore = 0.0
     for _, droneStack in ipairs(droneStackList) do
         -- This doesn't really happen in production, but it helps avoid some nasty recomputation in the simulator.
         if droneStack.individual == nil then
             goto continue
         end
 
-        -- TODO: Is this likely to converge? Are there better methods with higher probability or fewer generations?
-        -- TODO: We want to favor higher fertility of drones, but this doesn't actually do that since the number of offspring is only dependent
-        --       the princess's active fertility. We especially want to prevent using drones with 1 fertility whenever possible.
-        local chance = MatchingMath.CalculateChanceAtLeastOneOffspringIsPureBredTarget(target, princessStack.individual, droneStack.individual, cacheElement, traitInfo)
-        if chance > maxChance then
+        local score = scoreFunc(droneStack)
+        if score > maxScore then
             maxDroneStack = droneStack
-            maxChance = chance
-            if maxChance == 1 then
+            maxScore = score
+            if maxScore == maxPossibleScore then
                 break
             end
         end
         ::continue::
     end
-
-    -- TODO: We will accumulate more drones than we use during the operation of this breeder, so we need to garbage-collect the chest at some point.
 
     if maxDroneStack == nil then
         return -1
