3756905914a3c64db8d854782a41ac65835f311b
John Durrell <john.m.durrell@gmail.com>
2025-06-28 15:07:11 -0400

    Add s to deployment script.

--- deploy.sh
+++ deploy.sh
@@ -11,20 +11,24 @@ ROBOT_ID=$(<"$SCRIPT_DIR/robotid.txt")
 ANALYZER_ID=$(<"$SCRIPT_DIR/analyzerid.txt")
 
 # Copy server files to server.
-SERVER_DEST=$(echo $SAVE_FOLDER/opencomputers/$SERVER_ID/home/BeeBreederBot/)
+SERVER_DEST=$(echo $SAVE_FOLDER/opencomputers/$SERVER_ID/home/BeeBreederBot)
+mkdir -p "$SERVER_DEST"
 echo "Copying server files to $SERVER_DEST"
-cp "$SCRIPT_DIR"/BeeServer/* "$SERVER_DEST/BeeServer/"
-cp "$SCRIPT_DIR"/Shared/* "$SERVER_DEST/Shared"
+cp -r "$SCRIPT_DIR"/BeeServer "$SERVER_DEST"
+cp -r "$SCRIPT_DIR"/Shared "$SERVER_DEST"
 
 # Copy robot files to robot.
-ROBOT_DEST=$(echo $SAVE_FOLDER/opencomputers/$ROBOT_ID/home/BeeBreederBot/)
+ROBOT_DEST=$(echo $SAVE_FOLDER/opencomputers/$ROBOT_ID/home/BeeBreederBot)
 echo "Copying robot files to $ROBOT_DEST"
-cp "$SCRIPT_DIR"/BeekeeperBot/* "$ROBOT_DEST/BeekeeperBot"
-cp "$SCRIPT_DIR"/Shared/* "$ROBOT_DEST/Shared"
+mkdir -p "$ROBOT_DEST"
+cp -r "$SCRIPT_DIR"/BeekeeperBot "$ROBOT_DEST"
+cp -r "$SCRIPT_DIR"/Shared "$ROBOT_DEST"
 
 # Copy analyzer files to analyzer bot.
-ANALYZER_DEST=$(echo $SAVE_FOLDER/opencomputers/$ANALYZER_ID/home/BeeBreederBot/)
+ANALYZER_DEST=$(echo $SAVE_FOLDER/opencomputers/$ANALYZER_ID/home/BeeBreederBot)
 echo "Copying analyzer files to $ANALYZER_DEST"
-cp "$SCRIPT_DIR"/AnalyzerBot/* "$ANALYZER_DEST/AnalyzerBot"
+mkdir -p "$ANALYZER_DEST"
+mkdir -p "$ANALYZER_DEST/Shared"
+cp -r "$SCRIPT_DIR"/AnalyzerBot "$ANALYZER_DEST"
 cp "$SCRIPT_DIR"/Shared/Shared.lua "$ANALYZER_DEST/Shared"
 cp "$SCRIPT_DIR"/Shared/FieldDebug.lua "$ANALYZER_DEST/Shared"
